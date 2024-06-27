72b31dfacbc77926c43d890977622091b12c1db8
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-06-27 09:52:08 +0900

    having fun with lists

---
+++ lists.py
@@ -0,0 +1,69 @@
+# mylist = ["banana","cherry","apple"]
+
+# print(mylist)
+
+# item = mylist[-1]
+
+# for i in mylist:
+#     print(i)
+
+
+# if "banana" in mylist:
+#     print("Yes")
+# else:
+#     print("No")
+
+# mylist.append("lemon")
+# print(mylist)
+
+# mylist.insert(1,"blueberry")
+# print(mylist)
+
+# mylist.pop()
+# print(mylist)
+
+# item = mylist.remove("cherry")
+
+# item = mylist.clear()
+
+# item = mylist.reverse()
+
+# item = mylist.sort()
+# -----------------------------
+# mylist = [0] * 5
+# print(mylist)
+
+# list2 = [1,2,3,4]
+# new_list = mylist + list2
+# print(new_list)
+
+# ----------------------------
+
+# mylist = [1,2,3,4,5,6,7,8,9]
+
+# # a = mylist[1:5]
+# # print(a)
+
+# #reverse
+# a = mylist[::-1]
+# print(a)
+
+# ----------------------------
+# list_org = ["banana","cherry","apple"]
+
+# list_cpy = list_org
+
+# list_copy = list_org.copy() # don't change from the append()
+# list_copy = list(list_org)    # don't change from the append()
+# list_copy = list_org[:]   # don't change from the append()
+
+# list_cpy.append("lemon")
+
+# print(list_cpy)
+# print(list_org)
+
+#---------------------
+# a = [1,2,3,4,5]
+# b = [i*i for i in a]
+# print(a)
+# print(b)
