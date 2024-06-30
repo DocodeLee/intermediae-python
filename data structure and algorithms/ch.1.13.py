1fc66d528a45dd06dc431c26291ed1142328cf9e
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-06-30 14:32:16 +0900

    gcd fin

--- data structure and algorithms/ch.1.13.py
+++ data structure and algorithms/ch.1.13.py
@@ -1,65 +1,50 @@
-# class Fraction:
-#     def __init__(self,top,bottom):
-#         self.num = top
-#         self.den = bottom
-#         self.show()
+class Fraction:
+    def __init__(self,top,bottom):
+        self.num = top
+        self.den = bottom
+        self.show()
 
-#     def show(self):
-#         print(self.num , "/", self.den)
+    def show(self):
+        print(self.num , "/", self.den)
 
-# Fraction(3,5)
-
-# def gcd(m,n):
-#         while m % n !=0: # m= 22 n= 26
-#             oldm = m    # old m = 22
-#             oldn =n     #old n = 26
-            
-#             m =oldn     # m = 26
-#             n = oldm % oldn #n = 22 % 26
-            
-#         return n
-
-# class Fraction2:
-#     def __init__(self,top,bottom):
-#         self.num = top
-#         self.den = bottom
-    
-#     def __str__(self): # run without execution
-#         return str(self.num) + "/" + str(self.den)
-    
-#     def __add__(self,otherfraction):
-#         newnum = self.num*otherfraction.den + self.den*otherfraction.num
-#         newden = self.den*otherfraction.den
-#         common = gcd(newnum,newden)
-        
-#         return Fraction2(newnum//common , newden //common)
-    
-    
-            
-    
-#     def show(self):
-#         print(self.num , "/", self.den)
-
-# a = Fraction2(3,5)
-# print(a)
-
-# f1 = Fraction2(1,4)
-# f2 = Fraction2(1,2)
-# f3 = f1+f2
-# print(f3)
-
+Fraction(3,5)
 
 def gcd(m,n):
-    count = 0
-    while m % n !=0: # m= 22 n= 26
-        print(count)
-        oldm = m    # old m = 22
-        print("oldm = m ", oldm)
-        oldn =n     #old n = 26
-        print("oldn = n", oldn)
-        m =oldn     # m = 26
-        n = oldm % oldn #n = 22 % 26
-        count = count +1    
-    return n
+        while m % n !=0: # m= 22 n= 26
+            oldm = m    # old m = 22
+            oldn =n     #old n = 26
+            
+            m =oldn     # m = 26
+            n = oldm % oldn #n = 22 % 26
+            
+        return n
+
+class Fraction2:
+    def __init__(self,top,bottom):
+        self.num = top
+        self.den = bottom
     
-gcd(22,26)
+    def __str__(self): # run without execution
+        return str(self.num) + "/" + str(self.den)
+    
+    def __add__(self,otherfraction):
+        newnum = self.num*otherfraction.den + self.den*otherfraction.num
+        newden = self.den*otherfraction.den
+        common = gcd(newnum,newden)
+        
+        return Fraction2(newnum//common , newden //common)
+    
+    
+            
+    
+    def show(self):
+        print(self.num , "/", self.den)
+
+a = Fraction2(3,5)
+print(a)
+
+f1 = Fraction2(1,4)
+f2 = Fraction2(1,2)
+f3 = f1+f2
+print(f3)
+
